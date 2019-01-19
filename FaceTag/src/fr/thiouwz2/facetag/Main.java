58a8c1ec0e0596e47f001f82a2c04de9924c4fde
thiouwz2 <thiouwz2@192.168.0.32>
2019-01-19 21:20:29 +0100

    Initial test

---
+++ FaceTag/src/fr/thiouwz2/facetag/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package fr.thiouwz2.facetag;
+
+/**
+ * @author thiouwz2
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
